c945f82aeaf873c394cadab6e2b16feb8366cfc1
David Lundberg <davidlundberg99@gmail.com>
2022-06-14 21:42:43 -0400

    Implemented sliders on Rows

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import React, { useEffect, useState } from "react";
-import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, RowID }) => {
   const [movies, setMovies] = useState([]);
   const [like, setLike] = useState(false);
 
@@ -15,13 +15,29 @@ const Row = ({ title, fetchURL }) => {
     };
   }, [fetchURL]);
 
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + RowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + RowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-0 rounded absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + RowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-            // <Movie key={id} item={item} />
             <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
               <img
                 className="w-full h-auto block"
@@ -32,17 +48,22 @@ const Row = ({ title, fetchURL }) => {
                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                   {item?.title}
                 </p>
-                <p>
+                {/* <p>
                   {like ? (
                     <FaHeart className="absolute top-4 left-4" />
                   ) : (
                     <FaRegHeart className="absolute top-4 left-4" />
                   )}
-                </p>
+                </p> */}
               </div>
             </div>
           ))}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white right-0 rounded absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div>
       <Main />
-      <Row title="Upcoming" fetchURL={requests.requestUpcoming} />
-      <Row title="Popular" fetchURL={requests.requestPopular} />
-      <Row title="Trending" fetchURL={requests.requestTrending} />
-      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row RowID="1" title="Upcoming" fetchURL={requests.requestUpcoming} />
+      <Row RowID="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row RowID="3" title="Trending" fetchURL={requests.requestNowPlaying} />
+      <Row RowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
     </div>
   );
 };
